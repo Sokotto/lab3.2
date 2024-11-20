5687bc637ffd482981c734c124a6fb565e880a90
Xenzey <Xenzey@209-3>
2024-11-20 10:57:33 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите вещественное число A: ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите целое число N (<0): ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N >= 0)
+{
+    Console.WriteLine(" Ошибка: N должно быть меньше 0.");
+    return;
+}
+double result = CalculatePower(A, N);
+Console.WriteLine($"Результат A^{N}={result}");
+static double CalculatePower(double A, int N)
+{
+    int positiveN = -N;
+    double power = 1.0;
+    for (int i = 0; i < positiveN; i++)
+    {
+        power *= A;
+    }
+    return 1.0 / power;
+}
